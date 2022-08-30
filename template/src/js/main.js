f2e2f9fbb30cbf8e1e0c85c967b6a7199e651b5a
MINHTRI <tri.phamminh@drcomgroup.com>
2022-08-30 22:55:32 +0700

    update 30 Aug 2022

--- template/src/js/main.js
+++ template/src/js/main.js
@@ -1,24 +1,53 @@
-jQuery(document).ready(function (e) {
-    gnb_init();
+$(document).ready(function () {
 
-    function gnb_init() {
-        var jHamburger = $('.js-gnb-menu-btn'),
-          jClose = $('.gnb__menu-close'),
-          jFooterAccor = $('.accordian-item-header');
+  //-------------choose location-------------
+  var clickable = 1;
+  $('.location--placeholder').click(function(e){
+    if (clickable == 0)
+      return;
+      clickable = 0;
+    $('.location--list').slideToggle()
+    setTimeout(function(){
+      clickable = 1;
+    },1000);
+  })
 
-        jHamburger.on('click', function(e) {
-          e.preventDefault();
-          $('.gnb__menu-wrap, .gnb__depth1').addClass('open');
-        });
+  $('.location--item').click(function(){
+    let locale = $(this).data('locale');
+    let localeText = $(this).text();
 
-        jClose.on('click', function(e) {
-          e.preventDefault();
-          $('.gnb__menu-wrap, .gnb__depth1').removeClass('open');
-        });
+    $('.location--list').slideToggle()
+    $('.location').attr('data-locale', locale)
+    $('.location--placeholder').text(localeText)
+  })
 
-        jFooterAccor.on('click', function(e) {
-          e.preventDefault();
-          $(this).next().toggleClass('open');
-        });
-    };
+  $('.container').on('click', function(e){
+    if (!$(e.target).hasClass('location--placeholder') && !$(e.target).hasClass('location--item')) {
+      console.log('----1---')
+      if ($('.location--list').css('display') == 'block') {
+        $('.location--list').slideUp()
+      }
+    }
+  })
+
+  //--------------slick--------------------
+
+  let imgSlick = $('.carousel-wrapper').slick({
+    autoplay: true,
+    autoplaySpeed: 2000,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: true,
+    responsive: [{
+        breakpoint: 768,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            // arrows: false
+        }
+    }]
+  });
+
 });
